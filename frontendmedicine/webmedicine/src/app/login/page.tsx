b0b0e4cd9ee47614214cbbcf77278a90f3cac1e1
Breno Lopes <breno.skolegas07@gmail.com>
2025-04-10 23:44:43 -0300

    feat: add components shadcn and create loginpage

---
+++ frontendmedicine/webmedicine/src/app/login/page.tsx
@@ -0,0 +1,85 @@
+"use client"
+
+import { useState } from "react"
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import Logo from "@/components/Logo";
+
+
+export default function LoginPage(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const router = useRouter();
+
+    const handleLogin = async (e: React.FormEvent) =>{
+        e.preventDefault();
+        setError("");
+        
+        try{
+            const res = await fetch("http://localhost:5000/login", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ email, password }),
+            });
+
+            if(!res.ok) throw new Error ("Credenciais Invalidas!");
+
+            const data = await res.json();
+
+            if(!data.token){
+                throw new Error ("Token nao encontrado na resposta");
+            }
+            
+            const tokenString = data.token.token;
+            localStorage.setItem("token", tokenString);
+
+            router.push("/homepage")
+        } catch (err:any){
+            setError(err.message);
+        }
+    };
+
+    return (
+      <div className="flex flex-col min-h-screen min-w-screen items-center justify-center bg-gradient-to-r from-blue-400 to-blue-100">
+        <div className="mb-10">
+          <Logo></Logo>
+        </div>
+        <form
+          onSubmit={handleLogin}
+          className="bg-white p-6 rounded-lg shadow-md w-96"
+        >
+          <h2 className="text-2xl text-center font-bold mb-8">Login</h2>
+
+          {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
+
+          <Input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mb-4"
+            required
+          ></Input>
+          <Input
+            type="password"
+            placeholder="Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="mb-2"
+            required
+          ></Input>
+          <h3 className="text-sm mt-3 p-2">
+            Não possui uma conta{" "}
+            <span className="text-blue-900">Cadastre-se!</span>
+          </h3>
+          <Button type="submit" className="w-full mt-5">
+            Entrar
+          </Button>
+        </form>
+      </div>
+    );
+}
+
+
---
+++ frontendmedicine/webmedicine/src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div className="z-30 flex justify-center items-center mb-5">
+      <div className="bg-blue-500 flex mr-2 justify-center items-center rounded-full min-w-10 min-h-10">
+        <p className="text-white text-xl font-bold">W</p>
+      </div>
+      <h4 className="font-bold text-xl">Web Medicine</h4>
+    </div>
+  );
+}
