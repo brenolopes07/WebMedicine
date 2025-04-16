c7dc33ae014643f4a7281762bbaa54247f284de2
Breno Lopes <breno.skolegas07@gmail.com>
2025-04-16 18:04:20 -0300

    chore: update Logo component for better readability and performance

--- frontendmedicine/webmedicine/src/components/Logo.tsx
+++ frontendmedicine/webmedicine/src/components/Logo.tsx
@@ -1,12 +1,15 @@
+
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 export default function Logo() {
+  const router = useRouter();
   return (
-    <div className="z-30 flex justify-center items-center mb-5">
+    <div onClick={()=>router.push(`/homepage`)} className="z-30 flex justify-center items-center  cursor-pointer">
       <div className="bg-blue-500 flex mr-2 justify-center items-center rounded-full min-w-10 min-h-10">
         <p className="text-white text-xl font-bold">W</p>
       </div>
-      <h4 className="font-bold text-xl">Web Medicine</h4>
+      <h4 className="font-bold text-lg">Web Medicine</h4>
     </div>
   );
 }
