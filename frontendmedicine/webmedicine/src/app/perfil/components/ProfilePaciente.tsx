c10d6537b56f6f892d3f4b87ba6aa43b3ce41c0e
Breno Lopes <breno.skolegas07@gmail.com>
2025-04-25 01:27:30 -0300

    chore: continue implement interface page profile

--- frontendmedicine/webmedicine/src/app/perfil/components/ProfilePaciente.tsx
+++ frontendmedicine/webmedicine/src/app/perfil/components/ProfilePaciente.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Book, Pen, Pencil, User } from "lucide-react";
+import SearchConsultas from "./SearchConsultas";
+
 // Primeiro defina a interface/types
 type UsuarioPaciente = {
   name?: string;
@@ -23,13 +27,31 @@ export function ProfilePaciente({ usuario }: ProfilePacienteProps) {
 
 
   return (
-    <div className="space-y-4 p-6 max-w-xl mx-auto">
-      <h1 className="text-2xl font-bold">Olá, {usuario?.name}</h1>
-      <div className="space-y-2">
-        <p>
-          <strong>Telefone:</strong> {usuario?.phone}
-        </p>             
+    <>
+      <div className="space-y-4 p-6 max-w-xl mx-auto mt-10 flex gap-3 bg-gradient-to-l from-blue-400 to-blue-100  rounded-lg">
+        <User className="size-8"></User>
+        <h1 className="text-2xl font-bold">Olá, {usuario?.name}</h1>
       </div>
-    </div>
+      <div className="space-y-2 mt-5 p-6 max-w-xl mx-auto flex flex-col bg-gradient-to-l from-blue-400 to-blue-100 rounded-lg">
+        <div className="flex gap-2 items-center mb-4">
+          <Book></Book>
+          <h1 className="font-bold text-2xl">Seus Dados</h1>
+        </div>
+        <div className="flex flex-col">
+          <p className="font-bold">
+            Telefone: <span className="font-normal">{usuario?.phone}</span>
+          </p>
+          <p className="font-bold">
+            Endereco:<span className="font-normal"> {usuario?.adress}</span>
+          </p>
+        </div>
+        <Button className="bg-white mt-10 hover:bg-blue-600 text-black hover:text-white text-[15px]">
+          Editar dados
+        </Button>
+      </div>
+      <div className="space-y-2 mt-5 p-6 max-w-xl mx-auto flex flex-col bg-gradient-to-t from-blue-300 to-blue-100 rounded-lg mb-5">
+        <SearchConsultas />
+      </div>
+    </>
   );
 }
